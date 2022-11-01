66aedc11c3ce7571263adbdfb4514a615360df79
omprakashsridharan <omprakash.sridharan@sumup.com>
2022-11-01 15:25:45 +0100

    Adds set_get redis intg. test

--- src/redis.rs
+++ src/redis.rs
@@ -1,4 +1,4 @@
-use redis::{Client, RedisResult};
+use redis::{AsyncCommands, Client, RedisError, RedisResult};
 struct RedisHelper {
     client: Client,
 }
@@ -7,6 +7,18 @@ impl RedisHelper {
     pub fn new(redis_url: String) -> RedisResult<Self> {
         let client = redis::Client::open(redis_url)?;
         Ok(Self { client })
+    }
+
+    pub async fn set(&self, key: String, value: String) -> Result<(), RedisError> {
+        let mut connection = self.client.get_async_connection().await?;
+        connection.set(key, value).await?;
+        Ok(())
+    }
+
+    pub async fn get(&self, key: String) -> Result<String, RedisError> {
+        let mut connection = self.client.get_async_connection().await?;
+        let value: String = connection.get(key).await?;
+        Ok(value)
     }
 }
 
@@ -29,5 +41,23 @@ mod tests {
         let redis_url = format!("redis://127.0.0.1:{}", host_port);
         let redis_helper_result = RedisHelper::new(redis_url);
         assert_eq!(redis_helper_result.is_ok(), true);
+    }
+
+    #[tokio::test]
+    async fn set_get_works() {
+        let docker = clients::Cli::default();
+        let node: Container<Redis> = docker.run(Redis::default());
+        let host_port = node.get_host_port_ipv4(6379);
+        let redis_url = format!("redis://127.0.0.1:{}", host_port);
+        let redis_helper_result = RedisHelper::new(redis_url);
+        assert_eq!(redis_helper_result.is_ok(), true);
+        if let Ok(redis_helper) = redis_helper_result {
+            let set_result = redis_helper
+                .set("KEY".to_string(), "VALUE".to_string())
+                .await;
+            assert_eq!(set_result.is_ok(), true);
+            let get_result = redis_helper.get("KEY".to_string()).await;
+            assert_eq!(get_result, Ok("VALUE".to_string()))
+        }
     }
 }
