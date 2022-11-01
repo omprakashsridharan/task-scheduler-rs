a71c3e0df83375be324bb4bbe7965d051867c4f9
omprakashsridharan <omprakash.sridharan@sumup.com>
2022-11-01 16:54:53 +0100

    Adds set, get, delete, exists redis helper methods

--- src/redis.rs
+++ src/redis.rs
@@ -1,11 +1,12 @@
 use redis::{AsyncCommands, Client, RedisError, RedisResult};
+
 struct RedisHelper {
     client: Client,
 }
 
 impl RedisHelper {
     pub fn new(redis_url: String) -> RedisResult<Self> {
-        let client = redis::Client::open(redis_url)?;
+        let client = Client::open(redis_url)?;
         Ok(Self { client })
     }
 
@@ -20,36 +21,49 @@ impl RedisHelper {
         let value: String = connection.get(key).await?;
         Ok(value)
     }
+
+    pub async fn delete(&self, key: String) -> Result<(), RedisError> {
+        let mut connection = self.client.get_async_connection().await?;
+        connection.del(key).await?;
+        Ok(())
+    }
+
+    pub async fn exists(&self, key: String) -> Result<bool, RedisError> {
+        let mut connection = self.client.get_async_connection().await?;
+        let result = connection.exists(key).await?;
+        Ok(result)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::redis::RedisHelper;
+    use super::RedisHelper;
     use testcontainers::{clients, images::redis::Redis, Container};
 
     #[test]
-    fn redis_connection_invalid_throws_error() {
-        let redis_helper_result = RedisHelper::new("INVALID".to_string());
-        assert_eq!(redis_helper_result.is_err(), true)
-    }
-
-    #[test]
-    fn redis_connection_valid() {
+    fn returns_error_result_for_invalid_redis_connection_url() {
         let docker = clients::Cli::default();
         let node: Container<Redis> = docker.run(Redis::default());
         let host_port = node.get_host_port_ipv4(6379);
-        let redis_url = format!("redis://127.0.0.1:{}", host_port);
-        let redis_helper_result = RedisHelper::new(redis_url);
+        let redis_helper_result = RedisHelper::new("INVALID".to_string());
+        assert_eq!(redis_helper_result.is_err(), true);
+    }
+
+    #[test]
+    fn returns_valid_redis_connection_for_valid_url() {
+        let docker = clients::Cli::default();
+        let node: Container<Redis> = docker.run(Redis::default());
+        let host_port = node.get_host_port_ipv4(6379);
+        let redis_helper_result = RedisHelper::new(format!("redis://127.0.0.1:{}", host_port));
         assert_eq!(redis_helper_result.is_ok(), true);
     }
 
     #[tokio::test]
-    async fn set_get_works() {
+    async fn set_get_key_works() {
         let docker = clients::Cli::default();
         let node: Container<Redis> = docker.run(Redis::default());
         let host_port = node.get_host_port_ipv4(6379);
-        let redis_url = format!("redis://127.0.0.1:{}", host_port);
-        let redis_helper_result = RedisHelper::new(redis_url);
+        let redis_helper_result = RedisHelper::new(format!("redis://127.0.0.1:{}", host_port));
         assert_eq!(redis_helper_result.is_ok(), true);
         if let Ok(redis_helper) = redis_helper_result {
             let set_result = redis_helper
@@ -57,7 +71,54 @@ mod tests {
                 .await;
             assert_eq!(set_result.is_ok(), true);
             let get_result = redis_helper.get("KEY".to_string()).await;
-            assert_eq!(get_result, Ok("VALUE".to_string()))
+            assert_eq!(get_result, Ok("VALUE".to_string()));
+        }
+    }
+
+    #[tokio::test]
+    async fn delete_key_works() {
+        let docker = clients::Cli::default();
+        let node: Container<Redis> = docker.run(Redis::default());
+        let host_port = node.get_host_port_ipv4(6379);
+        let redis_helper_result = RedisHelper::new(format!("redis://127.0.0.1:{}", host_port));
+        assert_eq!(redis_helper_result.is_ok(), true);
+        if let Ok(redis_helper) = redis_helper_result {
+            let set_result = redis_helper
+                .set("KEY".to_string(), "VALUE".to_string())
+                .await;
+            assert_eq!(set_result.is_ok(), true);
+            let delete_result = redis_helper.delete("KEY".to_string()).await;
+            assert_eq!(delete_result.is_ok(), true);
+        }
+    }
+
+    #[tokio::test]
+    async fn returns_true_for_existing_key() {
+        let docker = clients::Cli::default();
+        let node: Container<Redis> = docker.run(Redis::default());
+        let host_port = node.get_host_port_ipv4(6379);
+        let redis_helper_result = RedisHelper::new(format!("redis://127.0.0.1:{}", host_port));
+        assert_eq!(redis_helper_result.is_ok(), true);
+        if let Ok(redis_helper) = redis_helper_result {
+            let set_result = redis_helper
+                .set("KEY".to_string(), "VALUE".to_string())
+                .await;
+            assert_eq!(set_result.is_ok(), true);
+            let delete_result = redis_helper.exists("KEY".to_string()).await;
+            assert_eq!(delete_result, Ok(true));
+        }
+    }
+
+    #[tokio::test]
+    async fn returns_true_for_non_existing_key() {
+        let docker = clients::Cli::default();
+        let node: Container<Redis> = docker.run(Redis::default());
+        let host_port = node.get_host_port_ipv4(6379);
+        let redis_helper_result = RedisHelper::new(format!("redis://127.0.0.1:{}", host_port));
+        assert_eq!(redis_helper_result.is_ok(), true);
+        if let Ok(redis_helper) = redis_helper_result {
+            let delete_result = redis_helper.exists("INVALID_KEY".to_string()).await;
+            assert_eq!(delete_result, Ok(false));
         }
     }
 }
